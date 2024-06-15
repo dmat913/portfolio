2b6c1487ac8f24a94e4d48e50c0abe299b1c6410
dmat913 <soccer.82.d.y@gmail.com>
2024-06-15 12:26:40 +0900

    feat:プロフィールカート初期表示作成

--- dmat-portfolio/src/pages/TopPage/TopPage.tsx
+++ dmat-portfolio/src/pages/TopPage/TopPage.tsx
@@ -7,7 +7,6 @@ import ProfileCard from '../../components/ProfileCard';
 function TopPage() {
   return (
     <div className={styles.root}>
-      <span className={styles.title}>ポートフォリオ</span>
       <a rel="noreferrer" target='_blank' href='https://github.com/dmat913'>
         <img src={GithubIcon} alt='GithubIcon' />
       </a>
